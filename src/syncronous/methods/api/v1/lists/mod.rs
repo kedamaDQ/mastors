4e0caca70396e59cc7c4893855014293f9a7ddcd
kedamaDQ <kedama@foresdon.jp>
2020-07-08 14:45:53 +0900

    Add method GET lists

--- src/syncronous/methods/api/v1/lists/mod.rs
+++ src/syncronous/methods/api/v1/lists/mod.rs
@@ -3,9 +3,16 @@ use serde::Serialize;
 use crate::{
 	Connection,
 	Method,
-	entities::List,
-	entities::Nothing,
+	entities::{ List, Lists, Nothing },
 };
+
+/// Get a request to get all your lists.
+pub fn get(conn: &Connection) -> GetLists {
+	GetLists {
+		conn,
+		authorized: true,
+	}
+}
 
 /// Get a request to create a new list timeline named by `title`.
 pub fn post(conn: &Connection, title: impl Into<String>) -> PostLists {
@@ -24,6 +31,21 @@ pub fn delete(conn: &Connection, id: impl Into<String>) -> DeleteLists {
 		id: id.into(),
 	}
 }
+
+/// GET request for `/api/v1/lists`.
+#[derive(Debug, Clone, Serialize, mastors_derive::Method)]
+#[method_params(GET, Lists, "/api/v1/lists")]
+pub struct GetLists<'a> {
+	#[serde(skip_serializing)]
+	#[mastors(connection)]
+	conn: &'a Connection,
+
+	#[serde(skip_serializing)]
+	#[mastors(authorization)]
+	authorized: bool,
+}
+
+impl<'a> Method<'a, Lists> for GetLists<'a> {}
 
 /// POST request for `/api/v1/lists`.
 #[derive(Debug, Clone, Serialize, mastors_derive::Method)]
@@ -73,6 +95,13 @@ mod tests {
 		let posted = post(&conn, title).send().unwrap();
 		assert_eq!(posted.title(), title);
 
+		let got = get(&conn).send().unwrap();
+		let expect_posted = got.iter()
+			.filter(|l| l.id() == posted.id())
+			.collect::<Vec<&List>>();
+		assert!(expect_posted.len() == 1);
+		assert_eq!(expect_posted.get(0).unwrap().title(), posted.title());
+		
 		println!("{:?}", delete(&conn, posted.id()).send());
 	}
 }
